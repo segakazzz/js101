743250897375a12fc3fa5b26549816eaa9e20fb7
sasatanikazue <sasatanikazue@sasatanikazues-MacBook-Pro.local>
2019-06-22 13:47:08 -0500

    Test passed

--- exercises/108-arrays.js
+++ exercises/108-arrays.js
@@ -10,7 +10,7 @@
 
 function threeFruits () {
   const fruits = ['Apple', 'Banana', 'Cherry']
-
+  return fruits
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,6 +19,7 @@ function threeFruits () {
 
 function multipleTypes () {
   const diverseArray = ['Skateboard', null, 8.75, 'Eiffel Tower', 44, 7, true, null]
+  return diverseArray
 
 }
 
@@ -39,6 +40,7 @@ function indexAccess () {
   console.assert(people[4] === 'Julia')
 
   // return the third item from the "people" array here
+  return people[2]
 
 }
 
@@ -48,6 +50,7 @@ function indexAccess () {
 
 function useLength () {
   const arr = ['a', 'b', 'c']
+  return arr.length
 
 }
 
@@ -57,7 +60,8 @@ function useLength () {
 
 function usePush () {
   let arr = ['a', 'b', 'c']
-
+  arr.push('d')
+  return arr
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -66,6 +70,8 @@ function usePush () {
 
 function usePop () {
   let arr = ['a', 'b', 'c']
+  let lastValue = arr.pop()
+  return arr
 
 }
 
@@ -84,8 +90,11 @@ function useIndexOf () {
   console.assert(arr.indexOf('Z') === -1) // no "Z" in this array
 
   // return the index of the first instance of "T" here
+  let index = arr.indexOf("T")
+  return index
 
 }
+
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Convert an Array into a string using .join()
@@ -100,6 +109,8 @@ function useJoin () {
   console.assert(arr.join('') === 'abcdef') // pass an empty string to have no separator
 
   // create and return the string 'a-b-c-d-e-f' here
+
+  return arr.join('-')
 
 }
 
